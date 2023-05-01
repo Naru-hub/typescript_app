4a522838fe48478e103993732cff41c42ccdf7ae
Naru-hub <1208ny0324@gmail.com>
2023-05-01 22:34:11 +0900

    内税と本体価格を合計するプログラムを作成

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
-console.log("Node path = " + process.argv[0])
-console.log("script file path = " + process.argv[1])
+console.log("Node path = " + process.argv[0]);
+console.log("script file path = " + process.argv[1]);
 
-const data: number[] = []
+const data: number[] = [];
 for (var i = 2; i < process.argv.length; i++) {
-  data.push(Number(process.argv[i]))
+  data.push(Number(process.argv[i]));
 }
-console.log(data)
+console.log("parameters: " + data);
+
+const f = aggregate()
 
 for (let item of data) {
-  const res = primeFactor(item)
-  console.log(item + '= ' + res)
+  const res = f(item);
+  console.log(res);
 }
 
-function primeFactor(a: number): number[] {
-  const v: number[] = []
-  let x = a
-  let n = 2
-  while (x > n) {
-    if (x % n == 0) {
-      x = x / n
-      v.push(n)
-    } else {
-      n += n == 2 ? 1 : 2
-    }
-  }
-  v.push(x)
-  return v
+function aggregate() : (n:number) => [number, number, number, number, number] {
+  let total = 0;
+  let totalp = 0;
+  let totalt = 0;
+  return (n:number) : [number, number, number, number, number] => {
+    total += n;
+    let tax = Math.floor(n - n / 1.1);
+    totalp += n - tax;
+    totalt += tax;
+    return [n, tax, total, totalp, totalt];
+  };
 }
